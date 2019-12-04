259a5092c22981446d78b28474af9a5cfa279c23
olusola olagoroye <solaolagoroye@gmail.com>
2019-12-04 02:28:32 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -40,7 +40,7 @@ def update_counting_hash(hash, key)
   if hash[key]
   hash[key] += 1
 else
-  puts "Key not found!"
+  hash[key] = 1
 end
 
 # given a hash an a key as parameters, return an updated hash
